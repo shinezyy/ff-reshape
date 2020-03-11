c016057b76cc84967a3e4c2416d9078a80de1d72
Gabe Black <gabeblack@google.com>
2020-03-10 18:14:56 -0700

    riscv: Implement translateFunctional.
    Change-Id: Ibe8adea8f66c7de22ee2ab0da54e866cd05fc257
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/26547
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Reviewed-by: Matthew Poremba <matthew.poremba@amd.com>
    Maintainer: Gabe Black <gabeblack@google.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- src/arch/riscv/tlb.hh
+++ src/arch/riscv/tlb.hh
@@ -116,6 +116,8 @@ class TLB : public BaseTLB
     void translateTiming(
             const RequestPtr &req, ThreadContext *tc,
             Translation *translation, Mode mode) override;
+    Fault translateFunctional(
+            const RequestPtr &req, ThreadContext *tc, Mode mode) override;
     Fault finalizePhysical(
             const RequestPtr &req,
             ThreadContext *tc, Mode mode) const override;
