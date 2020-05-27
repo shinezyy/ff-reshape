edd99a13b58863702263ebdc7c4b0c3409e2a2d0
Zhou Yaoyang <shinezyy@qq.com>
2020-05-27 18:32:03 +0800

    track sync.sh as a lib
    Change-Id: I58651d19200d3f1b60bf0fb50ccc0ba14e55f6bd

--- sync.sh
+++ sync.sh
@@ -1,97 +1,79 @@
-echo "syncing to 128"
-rsync -arzv --update \
-    --exclude=cmake-build-debug \
-    --exclude=.idea \
-    --exclude='*.pyc' \
-    --exclude='__pycache__' \
-    --exclude=build \
-    --exclude='*.swp'\
-    --exclude=sync.sh \
-    --exclude=util/run_sh_scrpits/common.py \
-    --exclude='parsetab.py' \
-    --no-owner --no-group \
-    --copy-unsafe-links \
-    ./ \
-    128:/home/zyy/projects/gem5/
-    # --exclude=.git \
-    # --exclude='.git*' \
-    # --exclude=util/run_sh_scrpits \
+sync_source_only () {
+    echo "syncing to $1:$2"
+    rsync -arzv --update \
+        --exclude=cmake-build-debug \
+        --exclude=.idea \
+        --exclude='*.pyc' \
+        --exclude='__pycache__' \
+        --exclude=build \
+        --exclude='*.swp'\
+        --exclude=SConstruct \
+        --exclude=util/run_sh_scrpits/common.py \
+        --exclude='parsetab.py' \
+        --no-owner --no-group \
+        --copy-unsafe-links \
+        --exclude=.git \
+        --exclude='.git*' \
+        --exclude=util/run_sh_scrpits \
+        ./ \
+        $1:$2
 
-echo "syncing to 130"
-rsync -arzv --update \
-    --exclude=cmake-build-debug \
-    --exclude=.idea \
-    --exclude='*.pyc' \
-    --exclude='__pycache__' \
-    --exclude=build \
-    --exclude='*.swp'\
-    --exclude=sync.sh \
-    --exclude=util/run_sh_scrpits/common.py \
-    --exclude='parsetab.py' \
-    --no-owner --no-group \
-    --copy-unsafe-links \
-    ./ \
-    130:/home/zhouyaoyang/projects/ff/
-    # --exclude=.git \
-    # --exclude='.git*' \
-    # --exclude=util/run_sh_scrpits \
+}
 
-echo "syncing to Dnode13"
-rsync -arzv --update \
-    --exclude=cmake-build-debug \
-    --exclude=.idea \
-    --exclude='*.pyc' \
-    --exclude='__pycache__' \
-    --exclude=build \
-    --exclude='*.swp'\
-    --exclude=sync.sh \
-    --exclude=util/run_sh_scrpits/* \
-    --exclude='parsetab.py' \
-    --no-owner --no-group \
-    --copy-unsafe-links \
-    ./ \
-    D13-RT:/home/zyy-work/ff/
-    # --exclude=.git \
-    # --exclude='.git*' \
-    # --exclude=util/run_sh_scrpits \
+sync_source_scripts () {
+    echo "syncing to $1:$2"
+    rsync -arzv --update \
+        --exclude=cmake-build-debug \
+        --exclude=.idea \
+        --exclude='*.pyc' \
+        --exclude='__pycache__' \
+        --exclude=build \
+        --exclude='*.swp'\
+        --exclude=SConstruct \
+        --exclude=util/run_sh_scrpits/common.py \
+        --exclude='parsetab.py' \
+        --no-owner --no-group \
+        --copy-unsafe-links \
+        --exclude=.git \
+        --exclude='.git*' \
+        ./ \
+        $1:$2
+}
 
+sync_source_scripts_git () {
+    echo "syncing to $1:$2"
+    rsync -arzv --update \
+        --exclude=cmake-build-debug \
+        --exclude=.idea \
+        --exclude='*.pyc' \
+        --exclude='__pycache__' \
+        --exclude=build \
+        --exclude='*.swp'\
+        --exclude=SConstruct \
+        --exclude=util/run_sh_scrpits/common.py \
+        --exclude='parsetab.py' \
+        --no-owner --no-group \
+        --copy-unsafe-links \
+        ./ \
+        $1:$2
+}
 
-
-echo "syncing to THU gold"
-rsync -arzv --update \
-    --exclude=cmake-build-debug \
-    --exclude=.idea \
-    --exclude='*.pyc' \
-    --exclude='__pycache__' \
-    --exclude=build \
-    --exclude='*.swp'\
-    --exclude=sync.sh \
-    --no-owner --no-group \
-    --copy-unsafe-links \
-    --exclude=util/run_sh_scrpits/* \
-    --exclude='parsetab.py' \
-    ./ \
-    fj-cpu:/home/auser/projects/ff/
-    # --exclude=util/run_sh_scrpits \
-    # --exclude=.git \
-    # --exclude='.git*' \
-
-# echo "syncing to Fuzhou gold"
-# rsync -arzv --update \
-#     --exclude=cmake-build-debug \
-#     --exclude=.idea \
-#     --exclude='*.pyc' \
-#     --exclude='__pycache__' \
-#     --exclude=build \
-#     --exclude='*.swp'\
-#     --exclude=sync.sh \
-#     --no-owner --no-group \
-#     --copy-unsafe-links \
-#     --exclude=util/run_sh_scrpits/* \
-#     --exclude='parsetab.py' \
-#     ./ \
-#     fuzhou-gold-28c-2.6GHz:/home/zyy/projects/ff/
-#     # --exclude=util/run_sh_scrpits \
-#     # --exclude=.git \
-#     # --exclude='.git*' \
+sync_source_git () {
+    echo "syncing to $1:$2"
+    rsync -arzv --update \
+        --exclude=cmake-build-debug \
+        --exclude=.idea \
+        --exclude='*.pyc' \
+        --exclude='__pycache__' \
+        --exclude=build \
+        --exclude='*.swp'\
+        --exclude=SConstruct \
+        --exclude=util/run_sh_scrpits/common.py \
+        --exclude='parsetab.py' \
+        --no-owner --no-group \
+        --copy-unsafe-links \
+        --exclude=util/run_sh_scrpits \
+        ./ \
+        $1:$2
+}
 
