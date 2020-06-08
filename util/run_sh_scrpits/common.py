0494ad061b59407227dfa61669f2119f4c9e016a
Zhou Yaoyang <shinezyy@qq.com>
2020-06-08 23:21:04 +0800

    scripts: let oracle bp run 199M instructions to avoid using up traces
    Change-Id: Ib4db5fe029d4973c224589b4039ea5554b7967dc

--- util/run_sh_scrpits/common.py
+++ util/run_sh_scrpits/common.py
@@ -85,6 +85,7 @@ def get_mem_demand(cpt_dir, cpt_id):
 class G5Config:
     def __init__(self, benchmark, bmk_outdir, cpt_id, arch,
                  full=True,
+                 full_max_insts=200 * 10**6,
                  cpu_model='OoO',
                  window_size=192,
                  debug=False,
@@ -108,7 +109,7 @@ class G5Config:
         self.task = ''
 
         if full:
-            self.insts = 220 * 10 ** 6
+            self.insts = full_max_insts
         else:
             self.insts = 19 * 10 ** 6
 
---
+++ util/run_sh_scrpits/labs/oracle_bp_d13.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('../')
+
+from os.path import join as pjoin
+from multiprocessing import Pool
+import common as c
+import local_config as lc
+
+num_thread = lc.cores_per_task
+full = True
+if full:
+    d = '-full'
+else:
+    d = ''
+outdir = f'{c.stats_base_dir}/branch_oracle{d}/'
+
+def main():
+    g5_configs = []
+
+    dict_options = {
+            '--use-bp': 'OracleBP',
+            '--branch-trace-file': 'useless_branch.protobuf.gz',
+            }
+    binary_options= [
+            '--check-outcome-addr',
+            '--branch-trace-en',
+            ]
+
+
+    #with open('./tmp.txt') as f:
+    with open('../all_function_spec2017.txt') as f:
+        for line in f:
+            if not line.startswith('#'):
+                for cpt_id in range(0, 3):
+                    benchmark = line.strip()
+                    task = benchmark + '_' + str(cpt_id)
+                    g5_config = c.G5Config(
+                        benchmark=benchmark,
+                        bmk_outdir=pjoin(outdir, task),
+                        cpt_id=cpt_id,
+                        arch='RISCV',
+                        full=full,
+                        full_max_insts=199 * 10**6,
+                        debug=False,
+                        debug_flags=[
+                            'Fetch',
+                            ],
+                        func_id='oracle_bp_test',
+                    )
+                    g5_config.add_options(binary_options)
+                    g5_config.update_options(dict_options)
+                    g5_configs.append(g5_config)
+
+    if num_thread > 1:
+        p = Pool(num_thread)
+        p.map(c.run_wrapper, g5_configs)
+    else:
+        g5_configs[0].check_and_run()
+
+
+if __name__ == '__main__':
+    main()
+
