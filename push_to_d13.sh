d33e23464da1df8af50e4a3af9e5bd77914e354e
Zhou Yaoyang <shinezyy@qq.com>
2020-09-08 11:47:11 +0800

    update syncing scripts
    Change-Id: Iff4e10a403a12399f79ae7e643b29b4e5042be69

---
+++ push_to_d13.sh
@@ -0,0 +1,11 @@
+. ./sync.sh
+while true;
+do
+    date '+%Y.%m.%d_%H:%M:%S'
+    push_source_scripts$2 D13-RT /home/zyy-work/ff;
+    if [ $1 -eq 0 ];
+    then
+        break
+    fi
+    sleep 60;
+done
