a4f886e4c1e967e74fda00620c10390122ba2f40
Zhou Yaoyang <shinezyy@qq.com>
2020-09-09 16:14:34 +0800

    enable directory wise diff test; 20M testing passed
    Change-Id: I455cc392666ce28b0a0009efb45c391becfffe53

--- util/python/diff_test.py
+++ util/python/diff_test.py
@@ -1,5 +1,10 @@
 import re
 import sys
+import os
+import os.path as osp
+from multiprocessing import Pool
+
+num_thread = 100
 
 assert len(sys.argv) == 3
 
@@ -16,31 +21,68 @@ def match_and_append(p, line):
     else:
         return None
 
-with open(sys.argv[1]) as f, open(sys.argv[2]) as golden_f:
-    count = 0
-    for lt, lg in zip(f, golden_f):
-        count += 1
-        if count % 10000 == 0:
-            print(count)
+def compare_files(test, gold, outf):
+    with open(test) as f, open(gold) as golden_f:
+        count = 0
+        for lt, lg in zip(f, golden_f):
+            count += 1
+            if count % 10000 == 0:
+                print(count, file=outf)
 
-        mt = match_and_append(pattern, lt)
-        mg = match_and_append(pattern, lg)
-        if mt is None and mg is None:
-            print('.')
-            continue
-        elif mt is None or mg is None:
-            print(f"Difference found in line {count}, one is None!")
+            mt = match_and_append(pattern, lt)
+            mg = match_and_append(pattern, lg)
+            if mt is None and mg is None:
+                print('.', file=outf)
+                continue
+            elif mt is None or mg is None:
+                print(f"Difference found in line {count}, one is None!", file=outf)
 
-        pc, val, addr = mt
-        pcg, valg, addrg = mg
+            pc, val, addr = mt
+            pcg, valg, addrg = mg
 
-        if pc != pcg or val != valg:
-            if val == '1' and valg == '3':
-                print('Ignore csrrs error')
-            else:
-                print(f"Difference found in line {count}")
-                print("test:", pc, val, "gold:", pcg, valg, "addr:", addr)
-                print("test:", lt, "gold:", lg, "addr:", addrg)
-                break
+            if pc != pcg or val != valg:
+                if val == '1' and valg == '3':
+                    print('Ignore csrrs error', file=outf)
+                else:
+                    print(f"Difference found in line {count}", file=outf)
+                    print("test:", pc, val, "gold:", pcg, valg, "addr:", addr, file=outf)
+                    print("test:", lt, "gold:", lg, "addr:", addrg, file=outf)
+                    break
+
+def compare_wrapper(l):
+    test, gold, fname = l
+    with open(fname, 'w') as f:
+        try:
+            compare_files(test, gold, f)
+        except Exception as e:
+            print(e)
+    return
+
+def main():
+    test = sys.argv[1]
+    gold = sys.argv[2]
+    if osp.isfile(test):
+        assert(osp.isfile(gold))
+        compare_files(test, gold, sys.stdout)
+    else:
+        assert(osp.isdir(test))
+
+        if not osp.exists('diff'):
+            os.makedirs('diff')
+
+        log_file = 'gem5_out.txt'
+        for d in os.listdir(test):
+            print(osp.join(test, d, log_file))
+            assert osp.isfile(osp.join(test, d, log_file))
+            assert osp.isfile(osp.join(gold, d, log_file))
+
+        p = Pool(num_thread)
+        args = []
+        for d in os.listdir(test):
+            args.append([osp.join(test, d, log_file), osp.join(gold, d, log_file),
+                    osp.join('diff', d)])
+        p.map(compare_wrapper, args)
 
 
+if __name__ == '__main__':
+    main()
