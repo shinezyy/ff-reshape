6d6060106c7796c7352c339cb67bfe06680e7adc
Zhou Yaoyang <shinezyy@qq.com>
2020-11-12 17:15:23 +0800

    Track script changins
    Change-Id: I86cf55f4899834dc3c3333a71dbdee04341fdd2a

--- util/run_sh_scrpits/local_config.py
+++ util/run_sh_scrpits/local_config.py
@@ -15,6 +15,12 @@ cpt_dirs = {
 stats_base_dir = osp.expanduser('~/gem5-results-2017')
 
 total_cores = 4
+
+dev_stats_base_dir = osp.expanduser('/vulnerable/zyy')
+
+branch_outcome_dir = osp.expanduser('~/research-data/spec2017_branch_outcome')
+
+simpoint_base_dir = osp.expanduser('~/gem5-results')
 n_tasks = 1
 spare_cores = 1
 
