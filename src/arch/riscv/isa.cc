5f68201b975bc9489c909778a3de2d111af750a1
Zhou Yaoyang <shinezyy@qq.com>
2021-01-24 15:58:31 +0800

    arch-riscv: fix sd bit maintaining
    Change-Id: I09c0ae2d8c1844d0fcbe7992d9ff6350686d9b6d

--- src/arch/riscv/isa.cc
+++ src/arch/riscv/isa.cc
@@ -388,6 +388,10 @@ ISA::setMiscReg(int misc_reg, RegVal val)
                 val &= ~(STATUS_SXL_MASK | STATUS_UXL_MASK);
                 val |= cur & (STATUS_SXL_MASK | STATUS_UXL_MASK);
                 setMiscRegNoEffect(misc_reg, val);
+
+                STATUS mstatus = readMiscRegNoEffect(misc_reg);
+                mstatus.sd = mstatus.xs == 3 || mstatus.fs == 3;
+                setMiscRegNoEffect(misc_reg, mstatus);
             }
             break;
           default:
