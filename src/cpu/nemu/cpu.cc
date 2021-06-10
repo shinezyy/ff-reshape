bf4839424ea72cb484bf8843385de6d3ffe06cdd
Zhou Yaoyang <shinezyy@qq.com>
2021-06-10 20:13:46 +0800

    cpu-nemu: setup the skeleton to drive caches.
    The major problems includes
    - how to obtain fetch paddr
    - how to make the cache completely dummy: record address only
    
    Change-Id: I02853cd3ab092d988033a3abea4e8b1ee9db7610

--- src/cpu/nemu/cpu.cc
+++ src/cpu/nemu/cpu.cc
@@ -4,7 +4,6 @@
 
 #include "cpu.hh"
 
-#include "cpu/nemu/include/protocal/instr_trace.h"
 #include "cpu/nemu/include/protocal/lockless_queue.h"
 
 std::thread *ExecutionThread;
@@ -32,8 +31,8 @@ NemuCPU::NemuCPU(const NemuCPUParams &params) :
         false, Event::CPU_Tick_Pri
         ),
         execCompleteEvent(nullptr),
-        dataPort(name() + ".dcache_port", this),
-        instPort(name() + ".icache_port", this)
+        icachePort(name() + ".dcache_port", this),
+        dcachePort(name() + ".icache_port", this)
 {
     extern void init_monitor(int argc, char *argv[]);
 
@@ -49,19 +48,43 @@ NemuCPU::NemuCPU(const NemuCPUParams &params) :
     threadContexts.push_back(tc);
 
     DPRINTF(NemuCPU, "Created the NemuCPU object\n");
-}
-
-bool NemuCPU::NemuCpuPort::recvTimingResp(PacketPtr pkt)
-{
-    return false;
-}
-
-void NemuCPU::NemuCpuPort::recvReqRetry()
-{
-
+    lastEntryType = -1;
 }
 
 static uint64_t cnt = 0;
+
+bool NemuCPU::dispatch(const ExecTraceEntry &entry)
+{
+    // manually pipeline jump address calculation and following instructions
+    if (__glibc_unlikely(lastEntryType == -1)) {
+        lastEntryType = entry.type;
+        lastEntry = entry;
+        return false; // just after startup
+    }
+
+    ExecTraceEntry tmpEntry = lastEntry;
+
+    switch (lastEntryType)
+    {
+    case ProtoInstType::MemRead:
+        // process fetch + load
+        processFetch(tmpEntry.fetchAddr);
+        processLoad(tmpEntry.memAddr);
+        break;
+    case ProtoInstType::MemWrite:
+        // process fetch + write
+        processFetch(tmpEntry.fetchAddr);
+        processStore(tmpEntry.memAddr);
+        break;
+    case ProtoInstType::EndOfStream:
+        return true;
+    default:
+        // process fetch
+        processFetch(tmpEntry.fetchAddr);
+        break;
+    }
+    return false;
+}
 
 void NemuCPU::tick()
 {
@@ -74,14 +97,20 @@ void NemuCPU::tick()
 
     for (int i = 10000; i > 0; i--) {
         ExecTraceEntry entry = traceQueue->pop();
-        if (entry.type == ProtoInstType::EndOfStream) {
+        // DPRINTF(NemuCPU, "fetchVaddr: 0x%#lx\n", entry.fetchAddr.v);
+        DPRINTF(NemuCPU,
+                "T = %d, FV = 0x%016X, FP = 0x%016X, MV = 0x%016X, MP = 0x%016X\n",
+                entry.type,
+                entry.fetchAddr.v,
+                entry.fetchAddr.p,
+                entry.memAddr.v,
+                entry.memAddr.p);
+        bool eos = dispatch(entry);
+        if (__glibc_unlikely(eos)) {
             schedule(*execCompleteEvent, curTick());
             break;
-        } else {
-
         }
     }
-
     reschedule(tickEvent, curTick() + clockPeriod(), true);
 }
 
@@ -96,6 +125,7 @@ void NemuCPU::init()
     tc->initMemProxies(tc);
     traceQueue = new LocklessConcurrentQueue<ExecTraceEntry>;
     execCompleteEvent = new CountedExitEvent("end of all NEMU instances.", nNEMU);
+    lastEntryType = -1;
 }
 
 void NemuCPU::activateContext(ThreadID tid)
@@ -114,4 +144,58 @@ void NemuCPU::activateContext(ThreadID tid)
     ExecutionThread = new std::thread(cpu_exec, -1);
 
     schedule(tickEvent, clockEdge(Cycles(0)));
+}
+
+void NemuCPU::setupFetchRequest(const RequestPtr &req)
+{
+    // TODO: impl
+}
+
+
+void NemuCPU::processFetch(const VPAddress &addr_pair)
+{
+    // TODO: impl
+    Packet pkt = Packet(ifetchReq, MemCmd::ReadReq);
+
+    pkt.dataStatic(&dummyInst);
+
+    sendPacket(icachePort, &pkt);
+    assert(!pkt.isError());
+}
+
+void NemuCPU::processLoad(const VPAddress &addr_pair)
+{
+    // TODO: impl
+
+    const RequestPtr &req = dataReadReq;
+    req->taskId(taskId());
+
+    Packet pkt(req, Packet::makeReadCmd(req));
+    pkt.dataStatic(dummyData);
+    sendPacket(dcachePort, &pkt);
+}
+
+void NemuCPU::processStore(const VPAddress &addr_pair)
+{
+    // TODO: impl
+    const RequestPtr &req = dataWriteReq;
+    req->taskId(taskId());
+    Packet pkt(req, Packet::makeWriteCmd(req));
+    pkt.dataStatic(dummyData);
+    sendPacket(dcachePort, &pkt);
+}
+
+void NemuCPU::sendPacket(RequestPort &port, const PacketPtr &pkt)
+{
+    port.sendAtomic(pkt);
+}
+
+Tick NemuCPU::AtomicCPUDPort::recvAtomicSnoop(PacketPtr pkt)
+{
+    panic("Snoop is not supported\n");
+}
+
+void NemuCPU::AtomicCPUDPort::recvFunctionalSnoop(PacketPtr pkt)
+{
+    panic("Snoop is not supported\n");
 }
--- src/cpu/nemu/cpu.hh
+++ src/cpu/nemu/cpu.hh
@@ -11,6 +11,7 @@
 #include <thread>
 
 #include "cpu/base.hh"
+#include "cpu/nemu/include/protocal/instr_trace.h"
 #include "cpu/simple_thread.hh"
 #include "debug/NemuCPU.hh"
 #include "params/NemuCPU.hh"
@@ -37,31 +38,27 @@ class NemuCPU: public BaseCPU
     uint64_t commitInstCount{};
 
   public:
-    class NemuCpuPort : public RequestPort
-    {
-      public:
-        NemuCpuPort(const std::string &_name, NemuCPU *_cpu)
-                : RequestPort(_name, _cpu), cpu(_cpu)
-        { }
+    // class NemuCpuPort : public RequestPort
+    // {
+    //   public:
+    //     NemuCpuPort(const std::string &_name, NemuCPU *_cpu)
+    //             : RequestPort(_name, _cpu), cpu(_cpu)
+    //     { }
 
-      protected:
-        /** KVM cpu pointer for finishMMIOPending() callback */
-        NemuCPU *cpu;
+    //   protected:
+    //     /** KVM cpu pointer for finishMMIOPending() callback */
+    //     NemuCPU *cpu;
 
-        bool recvTimingResp(PacketPtr pkt) override;
+    //     bool recvTimingResp(PacketPtr pkt) override;
 
-        void recvReqRetry() override;
-    };
+    //     void recvReqRetry() override;
+    // };
 
     /** Port for data requests */
-    NemuCpuPort dataPort;
+    // NemuCpuPort dataPort;
 
     /** Unused dummy port for the instruction interface */
-    NemuCpuPort instPort;
-
-    Port &getDataPort() override {return dataPort;}
-
-    Port &getInstPort() override {return instPort;}
+    // NemuCpuPort instPort;
 
     void wakeup(ThreadID tid) override;
 
@@ -72,6 +69,87 @@ class NemuCPU: public BaseCPU
     void tick();
 
     void activateContext(ThreadID thread_num) override;
+ private:
+
+   void setupFetchRequest(const RequestPtr &req);
+
+   bool dispatch(const ExecTraceEntry &entry);
+
+   ExecTraceEntry lastEntry;
+   int lastEntryType;
+
+   void processFetch(const VPAddress &addr_pair);
+
+   void processLoad(const VPAddress &addr_pair);
+
+   void processStore(const VPAddress &addr_pair);
+
+   void sendPacket(RequestPort &port, const PacketPtr &pkt);
+
+   /**
+     * An AtomicCPUPort overrides the default behaviour of the
+     * recvAtomicSnoop and ignores the packet instead of panicking. It
+     * also provides an implementation for the purely virtual timing
+     * functions and panics on either of these.
+     */
+   class AtomicCPUPort : public RequestPort
+   {
+
+   public:
+     AtomicCPUPort(const std::string &_name, NemuCPU*_cpu)
+         : RequestPort(_name, _cpu)
+     {
+     }
+
+   protected:
+     bool
+     recvTimingResp(PacketPtr pkt)
+     {
+       panic("Atomic CPU doesn't expect recvTimingResp!\n");
+     }
+
+     void
+     recvReqRetry()
+     {
+       panic("Atomic CPU doesn't expect recvRetry!\n");
+     }
+
+    };
+
+    class AtomicCPUDPort : public AtomicCPUPort
+    {
+
+      public:
+        AtomicCPUDPort(const std::string &_name, NemuCPU *_cpu)
+            : AtomicCPUPort(_name, _cpu), cpu(_cpu)
+        {
+            cacheBlockMask = ~(cpu->cacheLineSize() - 1);
+        }
+
+        bool isSnooping() const { return true; }
+
+        Addr cacheBlockMask;
+      protected:
+        NemuCPU *cpu;
+
+        virtual Tick recvAtomicSnoop(PacketPtr pkt);
+        virtual void recvFunctionalSnoop(PacketPtr pkt);
+    };
+
+    AtomicCPUPort icachePort;
+    AtomicCPUDPort dcachePort;
+    Port &getDataPort() override {return dcachePort;}
+    Port &getInstPort() override {return icachePort;}
+
+
+    RequestPtr ifetchReq;
+    RequestPtr dataReadReq;
+    RequestPtr dataWriteReq;
+    RequestPtr dataAmoReq;
+
+    TheISA::MachInst dummyInst;
+
+    uint8_t dummyData[64];
 };
 
 
