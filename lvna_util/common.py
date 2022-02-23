0477a430b70bb78ad8fcc2224c16a9f4410d1dcc
XiChen <chenxi171@mails.ucas.ac.cn>
2022-02-23 15:34:38 +0800

    lvna: add test scripts

---
+++ lvna_util/common.py
@@ -0,0 +1,18 @@
+benchmark_cpt_file = {
+             'gcc':'gcc_ref32_O5_22850000000_0.197858/0/_6850000000_.gz',
+             'gcc2':'gcc_ref32_O5_99150000000_0.170762/0/_3150000000_.gz',
+             'xal':'xalancbmk_144250000000_0.153516/0/_250000000_.gz',
+             'xal2':'xalancbmk_918450000000_0.10609/0/_6450000000_.gz',
+             'xal3':'xalancbmk_550750000000_0.12401/0/_6750000000_.gz',
+             'xal4':'xalancbmk_1041650000000_0.0838451/0/_1650000000_.gz'
+}
+
+# like 'gcc':'gcc_ref32_O5_22850000000_0.197858/
+benchmarks = {k:v.split('/')[0] for k,v in benchmark_cpt_file.items() }
+
+ff_base = '/nfs/home/chenxi/ff-reshape/'
+benchmark_dir = '/nfs/home/share/checkpoints_profiles/nemu_take_simpoint_cpt_17/'
+gcpt_bin_path = '/nfs/home/share/checkpoints_profiles/gcpt.bin'
+# ff_base = '/home/chenxi/ff-reshape'
+# benchmark_dir = '/home/zcq/lvna/5g/research-data/checkpoints_profiles/nemu_take_simpoint_cpt_17/'
+# gcpt_bin_path = '/home/zcq/lvna/5g/research-data/gcpt.bin'
---
+++ lvna_util/mix_nohype.py
@@ -0,0 +1,50 @@
+import gc
+import os
+import re
+import argparse
+import time
+from common import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-b','--benchmark', type=str, required=True,help="like gcc-xal-xal-xal")
+parser.add_argument('--insts',type=int,default=5_000_000)
+parser.add_argument('--warmup',type=int,default=50_000_000)
+parser.add_argument('--debug-flag',type=str)
+args = parser.parse_args()
+
+# ==================  Basics  ==================
+binary = ff_base+'build/RISCV/gem5.opt'
+outdir = ff_base+'log/{}'.format(args.benchmark)
+outopt = '--outdir='+outdir
+debugf = '--debug-flag='+ args.debug_flag if args.debug_flag else ''
+fspy   = ff_base+'configs/example/fs.py'
+
+# ==================  Options  ==================
+opt = []
+opt.append('--nohype --enable-loop-buffer --branch-trace-file=useless_branch.protobuf.gz')
+opt.append('--num-cpus=4 --cpu-type=DerivO3CPU --num-ROB=192 --num-PhysReg=192 --num-IQ=192 --num-LQ=72 --num-SQ=48')
+opt.append('--mem-type=DDR4_2400_16x4 --mem-size=32GB')
+
+opt.append('--cacheline_size=64')
+opt.append('--caches --l2cache --l3_cache')
+opt.append('--l1i_size=64kB --l1i_assoc=4')
+opt.append('--l1d_size=32kB --l1d_assoc=8')
+
+opt.append('--l2_size=768kB --l2_assoc=12')
+opt.append('--l3_size=2MB --l3_assoc=8')
+
+gcpt_all = [(benchmark_dir + benchmark_cpt_file[bm]) for bm in args.benchmark.split("-")]
+opt.append('--job-benchmark')
+
+# use "" around multiple paths connnected by ;
+opt.append('--generic-rv-cpt=' + '"' + ";".join(gcpt_all) + '"')
+opt.append('--gcpt-restorer=' + gcpt_bin_path)
+
+opt.append('--maxinsts={} --gcpt-warmup={}'.format(args.insts+args.warmup, args.warmup))
+
+# ==================  RUN  ==================
+cmd = [binary, outopt, debugf, fspy]
+cmd.extend(opt)
+print(" ".join(cmd))
+os.system(" ".join(cmd))
+os.system("echo "+time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())+">"+outdir+"/timestamp")
