992cc4d951bee18cf7d8116ee069ba0b8383c55f
cassuto <diyer175@hotmail.com>
2022-03-21 14:52:03 +0800

    ff-bp: update report path and BP parameters for profiling.
    Change-Id: I50213e6bf70504c0cb00fb4999151e3d04064ad3

--- profile-ff-bp.sh
+++ profile-ff-bp.sh
@@ -4,31 +4,49 @@ TARGETS=("/nfs/home/share/checkpoints_profiles/spec06_rv64gc_o2_20m/take_cpt/ast
         "/nfs/home/share/checkpoints_profiles/spec06_rv64gc_o2_20m/take_cpt/gcc_200_146840000000_0.021399/0/_146840000000_.gz"
         "/nfs/home/share/checkpoints_profiles/spec06_rv64gc_o2_20m/take_cpt/bzip2_program_370340000000_0.020905/0/_370340000000_.gz"
         "/nfs/home/share/checkpoints_profiles/spec06_rv64gc_o2_20m/take_cpt/libquantum_1003320000000_0.007505/0/_1003320000000_.gz")
+MAXINSTS=5000000
 
+ts=`date +%y-%d-%m-%T`
 if [ ! -d "./rpt" ]; then
-   mkdir ./rpt
+    mkdir ./rpt
 fi
+mkdir ./rpt/${ts}
 
 count=0
+
+# $1: count
+# $2: name of checkpoint
+# $3: BP type
+write_rpt () {
+    fn=./rpt/${ts}/$1.$3.rpt
+    echo "Checkpoint: $2" > ${fn}
+    date >> ${fn}
+	grep ffBranchPred m5out/stats.txt >> ${fn}
+}
 
 for ckp in ${TARGETS[*]}; do
     echo "#${count} Running  ${ckp}"
 
     ./build/RISCV/gem5.opt --debug-flags=ForwardN ./configs/example/fs.py \
-		--mem-size=8GB --maxinsts 5000000 \
+		--mem-size=8GB --maxinsts ${MAXINSTS} \
 		--generic-rv-cpt ${ckp} \
 		--caches --cpu-type=DerivO3CPU \
 		--ff-bp-type=forwardN \
         --trace-forward-n 1000000:100 \
-        --forward-n-numGTabBanks 4 \
+        --forward-n-numGTabBanks 6 \
         --forward-n-numGTabEntries 1024 \
-        --forward-n-numBTabEntries 4096 \
+        --forward-n-numBTabEntries 8192 \
         --forward-n-histLenInitial 10 \
         --forward-n-histLenGrowth 2 \
         --forward-n-randNumSeed 0
+    write_rpt ${count} ${ckp} forward
 
-    echo "Checkpoint: ${ckp}" > ./rpt/${count}.rpt
-    date >> ./rpt/${count}.rpt
-	grep ffBranchPred m5out/stats.txt >> ./rpt/${count}.rpt
+    ./build/RISCV/gem5.opt --debug-flags=ForwardN ./configs/example/fs.py \
+		--mem-size=8GB --maxinsts ${MAXINSTS} \
+		--generic-rv-cpt ${ckp} \
+		--caches --cpu-type=DerivO3CPU \
+		--ff-bp-type=trivial
+    write_rpt ${count} ${ckp} trivial
+
     let count++
 done
